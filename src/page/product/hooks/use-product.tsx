6f947bd9768da1304b1c26d7926cb349e6d7a1b8
matheus369k <gomes.craft10@gmail.com>
2025-06-16 18:55:01 -0300

    refactor: use-product.tsx - remove logic of the restore old data

--- src/page/product/hooks/use-product.tsx
+++ src/page/product/hooks/use-product.tsx
@@ -1,30 +1,17 @@
-import { addSelectProduct, removeSelectProduct } from '@/redux/product/slice';
+import { removeSelectProduct } from '@/redux/product/slice';
 import { ProductAmountContext } from '../contexts/products-amount';
 import { addCartProducts } from '@/redux/cart/slice';
 import { SliceProductCartType } from '@/@types/product';
-import { appUseSelector } from '@/redux/hook';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { useDispatch } from 'react-redux';
 import { useRedirect } from '@/hooks';
 
 export const useProduct = () => {
   const { handleAddProduct, handleRemoveProduct, productsAmount, handleResetProducts } =
     useContext(ProductAmountContext);
-  const { selected } = appUseSelector((state) => state.product);
   const { handleBackPage } = useRedirect();
 
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (selected) return;
-
-    const productCacheData = JSON.parse(localStorage.getItem('selectedProduct'));
-
-    if (productCacheData) {
-      dispatch(addSelectProduct(productCacheData));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const handleBuyProduct = ({ data }: Omit<SliceProductCartType, 'quantity'>) => {
     dispatch(
@@ -41,7 +28,6 @@ export const useProduct = () => {
 
   const handleRemoveStoreProduct = () => {
     handleBackPage();
-
     dispatch(removeSelectProduct());
   };
 
