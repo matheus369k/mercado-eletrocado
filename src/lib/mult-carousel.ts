cce5ca7bed2b67b1caca43a8939d45fa5bfb065e
matheus369k <gomes.craft10@gmail.com>
2025-06-16 18:21:03 -0300

    feat: mult-carousel.ts content variables with responsive props from carousel horizontal

---
+++ src/lib/mult-carousel.ts
@@ -0,0 +1,14 @@
+export const MultiCarouselHorizonResponsive = {
+  desktop: {
+    breakpoint: { min: 1024, max: 3000 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { min: 669, max: 1024 },
+    items: 3,
+  },
+  mobile: {
+    breakpoint: { min: 300, max: 669 },
+    items: 2,
+  },
+};
