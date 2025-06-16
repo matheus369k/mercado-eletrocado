7c06bd7fad11d121e039b45b5f0327c80837f7b6
matheus369k <gomes.craft10@gmail.com>
2025-06-16 18:16:14 -0300

    feat: FieldForm - create component to field from page login and register

---
+++ src/page/user/components/FormField/index.tsx
@@ -0,0 +1,20 @@
+import type { ComponentProps } from 'react';
+import styles from './index.module.css';
+import { useFormContext } from 'react-hook-form';
+
+interface FormFieldRootProps extends ComponentProps<'div'> {
+  customClass?: string;
+}
+
+export const FormFieldRoot = ({ customClass = '', ...props }: FormFieldRootProps) => {
+  return <div {...props} className={`${styles.form_field_container} ${styles[customClass]}`} />;
+};
+
+interface FormFieldInputProps extends Omit<ComponentProps<'input'>, 'name'> {
+  name: string;
+}
+
+export const FormFieldInput = ({ ...props }: FormFieldInputProps) => {
+  const { register } = useFormContext();
+  return <input {...props} {...register(props.name)} />;
+};
