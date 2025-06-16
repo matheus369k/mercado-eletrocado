9f063f7fe05098c133e8fd76302f3ee60239b4eb
matheus369k <gomes.craft10@gmail.com>
2025-06-16 17:55:20 -0300

    feat: const - add new and update old variables...
    - rename variables from LOCAL_STORAGE_KEYS to BROWSER_STORAGE_KEYS
    - add variable with keys of the cookies
    - add more key to BROWSER_STORAGE
    - add variables AUTO_CONNECTION, used for auto login
    - add variables ROUTES_PATHNAMES, used for the routes

--- src/util/const.ts
+++ src/util/const.ts
@@ -1,11 +1,35 @@
+import { browserLocalStorage, browserSessionStorage } from './browser-storage';
+
 export const CATEGORY_PRODUCTS_TYPES = {
   notebook: 'Notebook',
   tablet: 'Tablet',
   phone: 'Celular',
 };
 
-export const LOCAL_STORAGE_KEYS = {
+export const BROWSER_STORAGE_KEYS = {
   SELECTED_PRODUCT: 'ELETROCADO__PRODUCT__SELECT_PRODUCT',
   CART_PRODUCT: 'ELETROCADO__CART__CART_PRODUCT',
   ENVOY_PRODUCT: 'ELETROCADO__ENVOY__ENVOY_PRODUCT',
+  FAVORITE_PRODUCT: 'ELETROCADO__FAVORITE__FAVORITE_PRODUCT',
+  AUTO_CONNECTION: 'ELETROCADO__PERMISSION__AUTO_CONNECTION',
+  PERCENT_DISCOUNT: 'ELETROCADO__DISCOUNT_PRODUCT__PERCENT_DISCOUNT',
+};
+
+export const COOKIES_KEYS = {
+  USER_DATAS: 'ELETROCADO__USER__USER_DATAS',
+};
+
+export const AUTO_CONNECTION =
+  browserLocalStorage.get(BROWSER_STORAGE_KEYS.AUTO_CONNECTION) ||
+  browserSessionStorage.get(BROWSER_STORAGE_KEYS.AUTO_CONNECTION);
+
+export const ROUTES_PATHNAMES = {
+  HOME: '/mercado-eletrocado',
+  CAR: '/mercado-eletrocado/car',
+  CHECKED: '/mercado-eletrocado/car/checkout',
+  USER: '/mercado-eletrocado/user',
+  USER_PROFILER: '/mercado-eletrocado/user/profile',
+  USER_LOGIN: '/mercado-eletrocado/user/login',
+  USER_REGISTER: '/mercado-eletrocado/user/register',
+  PRODUCT: '/mercado-eletrocado/product/:productId',
 };
