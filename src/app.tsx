a98475a4750a609028f8024bd2fe2d3179869c8c
matheus369k <gomes.craft10@gmail.com>
2025-06-16 18:07:51 -0300

    feat: update route file
    - renamed file
    - import in app file
    - modify location of the file with routes paths

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
-import { routes } from './routes/PagesRoutes';
+import { routes } from './routes';
 import './styles/index.css';
 
 export function App() {
---
+++ src/routes/index.tsx
@@ -0,0 +1,30 @@
+import {
+  UserLogin,
+  UserProfile,
+  UserRegister,
+  StoreCart,
+  CheckedPage,
+  Home,
+  ProductPage,
+} from '@/page';
+import { createBrowserRouter } from 'react-router-dom';
+import { ErrorRoute } from '@/components';
+import { Layout } from '@/root/layout';
+import { ROUTES_PATHNAMES } from '@/util/const';
+
+export const routes = createBrowserRouter([
+  {
+    path: ROUTES_PATHNAMES.HOME,
+    element: <Layout />,
+    errorElement: <ErrorRoute />,
+    children: [
+      { path: ROUTES_PATHNAMES.HOME, element: <Home /> },
+      { path: ROUTES_PATHNAMES.CAR, element: <StoreCart /> },
+      { path: ROUTES_PATHNAMES.CHECKED, element: <CheckedPage /> },
+      { path: ROUTES_PATHNAMES.USER_PROFILER, element: <UserProfile /> },
+      { path: ROUTES_PATHNAMES.USER_LOGIN, element: <UserLogin /> },
+      { path: ROUTES_PATHNAMES.USER_REGISTER, element: <UserRegister /> },
+      { path: ROUTES_PATHNAMES.PRODUCT, element: <ProductPage /> },
+    ],
+  },
+]);
