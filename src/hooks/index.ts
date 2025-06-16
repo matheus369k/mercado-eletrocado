6adf10870e4ee8d29ad6849328632d55d796640f
matheus369k <gomes.craft10@gmail.com>
2025-06-16 18:23:16 -0300

    refactor: index file from global hooks - remove export of the custom hooks deleted and renamed

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,3 @@
 export { useFavoriteProduct } from './use-favorite-product';
-export { useRedirectionPage } from './use-redirection-page';
-export { useStockProduct } from './use-stock-product';
 export { useSelectProduct } from './use-add-product';
-export { useGetProducts } from './use-get-products';
-export { useAutoLogin } from './use-auto-login';
 export { useRedirect } from './use-redirect';
-export { useSlide } from './use-slider';
