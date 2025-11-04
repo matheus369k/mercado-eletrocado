0e5fdc8bbcf01d964b07824f30962ece048ef224
matheus369k <gomes.craft10@gmail.com>
2025-11-04 17:42:53 -0300

    styles(priceStockInfo): formatted all prices of the products

--- src/components/PriceStockInfo/index.tsx
+++ src/components/PriceStockInfo/index.tsx
@@ -1,19 +1,13 @@
 import { calcDiscountOfProductPrice } from '@/util/calc-discount';
 import { ProductType } from '@/@types/product';
 import styles from './index.module.css';
-import { useLocation } from 'react-router-dom';
-import { ROUTES_PATHNAMES } from '@/util/const';
 
 interface PriceStockInfoProps extends Pick<ProductType, 'price' | '_id'> {
   customClass?: string;
 }
 
 export const PriceStockInfo = ({ price, _id, customClass }: PriceStockInfoProps) => {
-  const { pathname } = useLocation();
-  let formattedPrice = price;
-  if (pathname.includes(ROUTES_PATHNAMES.USER)) {
-    formattedPrice = price / 100;
-  }
+  const formattedPrice = price / 100;
   const priceInfo = calcDiscountOfProductPrice(50, formattedPrice, _id);
 
   const priceWithoutDiscount = 'R$' + priceInfo.priceDiscount.toFixed(2);
