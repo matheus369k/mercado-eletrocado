8e84f06fa20de734afc5399fe96181dcc687f43e
matheus369k <gomes.craft10@gmail.com>
2025-11-17 18:40:20 -0300

    test(configs): config environment for run test

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    exclude: ['node_modules/*', 'dist/*'],
+    alias: { '@': path.resolve(__dirname, 'src') },
+    environmentOptions: {
+      jsdom: {
+        url: 'http://localhost:5173',
+      },
+    },
+    testTimeout: 10000,
+    env: process.env,
+    clearMocks: true,
+    mockReset: true,
+    coverage: {
+      provider: 'v8',
+      clean: true,
+    },
+  },
+});
