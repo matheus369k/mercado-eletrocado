886765bc35f1c725a1d8aa1b3f7da668eeaaead4
matheus369k <gomes.craft10@gmail.com>
2025-12-01 17:14:36 -0300

    test(env): add env variables to test environment

--- src/vitest.setup.ts
+++ src/vitest.setup.ts
@@ -1,6 +1,12 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
+import { afterEach, vi } from 'vitest';
+
+vi.mock('@/env.ts', () => ({
+  env: {
+    VITE_DATABASE_URL: 'http://localhost:3000',
+  },
+}));
 
 afterEach(() => {
   cleanup();
