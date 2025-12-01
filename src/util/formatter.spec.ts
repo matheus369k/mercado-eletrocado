1d1c115901cfa7e4f6e108c87be9e71b68616419
matheus369k <gomes.craft10@gmail.com>
2025-12-01 16:52:55 -0300

    test(util/formatter): resolver error when testing with another timezone

--- src/util/formatter.spec.ts
+++ src/util/formatter.spec.ts
@@ -1,9 +1,14 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { formatter } from './formatter';
+import { faker } from '@faker-js/faker/locale/pt_BR';
 
 describe('formatted function', () => {
-  it('should formatted date how 31/12/1969', () => {
-    const date = new Date(0).toString();
-    expect(formatter.dateDefault(date)).toBe('31/12/1969');
+  const formattedDateRegex =
+    /^(?:([0-2]?\d|3[01])\/(0?\d|1[0-2])\/\d{4}|(0?\d|1[0-2])\/([0-2]?\d|3[01])\/\d{4})$/;
+
+  it('should formatted date as DD/MM/YYYY or MM/DD/YYYY', () => {
+    const formattedDate = formatter.dateDefault(faker.date.past().toISOString());
+
+    expect(formattedDateRegex.test(formattedDate)).toBeTruthy();
   });
 });
